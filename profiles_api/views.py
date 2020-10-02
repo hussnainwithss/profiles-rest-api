02f0ed6a92d646049751f699658417a5dd05779f
hussnainwithss <21100150@lums.edu.pk>
2020-10-02 16:37:26 +0500

    added a test hello api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+
+class HelloApiView(APIView):
+    """ helloworld api view for testing"""
+
+    def get(self, request, format=None):
+        """Returns response of the get response to API"""
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'is similar to a traditional Django view',
+            'Gives you most control over your app logic'
+        ]
+        return Response({'message': 'Django REST API', 'an_apiview': an_apiview})
